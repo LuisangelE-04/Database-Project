8439ee5dc2c9448c25e910013cb7556d44109d7e
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2024-10-14 18:25:00 -0500

    add styles to footer, still a work a progress

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,15 +4,27 @@ import '../css/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2024 Post Office. All rights reserved.</p>
+      <div>
+        <span className="footer-titles">This Site</span>
         <nav>
           <ul>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="">Tracking</a></li>
+            <li><a href="">Shipment</a></li>
+            <li><a href="">About</a></li>
           </ul>
         </nav>
+      </div>
+      <div>
+      <span className="footer-titles">Other Sites</span>
+        <nav>
+          <ul>
+            <li><a href="">Post Office Store</a></li>
+            <li><a href="">Employee Login</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="footer-content">
+        <p>&copy; 2024 Post Office. All rights reserved.</p>
       </div>
     </footer>
   );
