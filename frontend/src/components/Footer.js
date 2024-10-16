4ce90077dba3f3c76e299d71ceae454f2ce56179
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2024-10-15 21:24:11 -0500

    add polished draft of footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,30 +3,40 @@ import '../css/Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div>
-        <span className="footer-titles">This Site</span>
-        <nav>
-          <ul>
-            <li><a href="">Tracking</a></li>
-            <li><a href="">Shipment</a></li>
-            <li><a href="">About</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-      <span className="footer-titles">Other Sites</span>
-        <nav>
-          <ul>
-            <li><a href="">Post Office Store</a></li>
-            <li><a href="">Employee Login</a></li>
-          </ul>
-        </nav>
-      </div>
-      <div className="footer-content">
-        <p>&copy; 2024 Post Office. All rights reserved.</p>
-      </div>
-    </footer>
+    <div className="footer-wrapper">
+      <footer className="footer">
+        <div className="footer-columns">
+          <div className="footer-content">
+            <div className="footer-links">
+              <span className="footer-titles">This Site</span>
+              <nav>
+                <ul>
+                  <li><a href="#">Tracking</a></li>
+                  <li><a href="#">Shipment</a></li>
+                  <li><a href="#">About</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className="footer-content">
+            <div className="footer-links">
+              <span className="footer-titles">Other Sites</span>
+              <nav>
+                <ul>
+                  <li><a href="#">Post Office Store</a></li>
+                  <li><a href="#">Employee Login</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="footer-extras">
+          <div className="footer-section">
+            <p>&copy; 2024 Post Office. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
