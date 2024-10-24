443dfeb7cb23ffea98f7d562f3357dc4e2480e43
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2024-10-23 19:32:22 -0500

    add endpoints.js

---
+++ src/endpoints/Endpoints.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const BASE_URL = 'http://localhost:5000';
+
+export const ENDPOINTS = {
+
+};
+
+export const createENDPOINT = (endpoint) => {
+  let url = BASE_URL;
+
+  return [
+    
+  ]
+}
